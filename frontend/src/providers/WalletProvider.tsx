ddec333ff004a7b35bcdb3da22649abdbe484cf5
Pratiikpy <apkadost888@gmail.com>
2026-02-16 16:29:18 +0530

    fix: support both Shield Wallet and Leo Wallet adapters
    Users may have either version of the extension installed.

--- frontend/src/providers/WalletProvider.tsx
+++ frontend/src/providers/WalletProvider.tsx
@@ -6,6 +6,7 @@ import { WalletModalProvider } from '@provablehq/aleo-wallet-adaptor-react-ui'
 import { DecryptPermission } from '@provablehq/aleo-wallet-adaptor-core'
 import { Network } from '@provablehq/aleo-types'
 import { ShieldWalletAdapter } from '@provablehq/aleo-wallet-adaptor-shield'
+import { LeoWalletAdapter } from '@provablehq/aleo-wallet-adaptor-leo'
 import '@provablehq/aleo-wallet-adaptor-react-ui/dist/styles.css'
 import { APP_NAME, PROGRAM_ID } from '@/lib/config'
 
@@ -17,6 +18,7 @@ export const WalletProvider: FC<Props> = ({ children }) => {
   const wallets = useMemo(
     () => [
       new ShieldWalletAdapter({ appName: APP_NAME }),
+      new LeoWalletAdapter({ appName: APP_NAME }),
     ],
     []
   )
